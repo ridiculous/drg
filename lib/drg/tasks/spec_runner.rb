d455445d9ec0decb47f4e54f2f81c34f7e670fd5
Ryan Buckley <arebuckley@gmail.com>
2015-11-22 17:50:17 -0800

    refactor spec runner to not generate the spec if the file already exists :sweat_smile:

--- lib/drg/tasks/spec_runner.rb
+++ lib/drg/tasks/spec_runner.rb
@@ -15,23 +15,26 @@ module DRG
       def perform
         fail ArgumentError, %Q(File or directory does not exist: "#{file}") if !File.exists?(file) && !File.exists?("#{file}.rb")
         ruby_files.each do |ruby_file|
-          file_path = Pathname.new(File.expand_path(ruby_file))
-          spec = DRG::Spec.generate(file_path)
-          unless spec
-            log "- #{ruby_file} - no methods", :gray
-            next
-          end
           rspec_file = Pathname.new(spec_file(ruby_file))
           spec_file_path = rspec_file.to_s[%r|/(spec/.+)|, 1]
-          if rspec_file.exist?
-            log "- #{spec_file_path} - already exists", :gray
-            next
-          end
+          next if rspec_file.exist?.tap { |result| log "- #{spec_file_path} - already exists", :gray if result }
+          spec = generate_spec(ruby_file)
+          next unless spec
           log "+ #{spec_file_path}"
           FileUtils.mkdir_p(rspec_file.parent)
           File.open(rspec_file, 'wb') do |f|
             f << spec.join("\n")
           end
+        end
+      end
+
+      def generate_spec(ruby_file)
+        spec = DRG::Spec.generate Pathname.new(File.expand_path(ruby_file))
+        if spec
+          spec
+        else
+          log "- #{ruby_file} - no methods", :gray
+          nil
         end
       end
 
