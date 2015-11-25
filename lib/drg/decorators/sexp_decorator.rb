b25f87d1c1da31f2c2548d2344526d71bc427586
Ryan Buckley <arebuckley@gmail.com>
2015-11-24 22:12:00 -1000

    remove unnecessary line

--- lib/drg/decorators/sexp_decorator.rb
+++ lib/drg/decorators/sexp_decorator.rb
@@ -2,7 +2,6 @@ class DRG::Decorators::SexpDecorator < DelegateClass(Sexp)
   def each_sexp_condition
     return enum_for(__method__) unless block_given?
     each_sexp do |exp|
-      next unless exp.is_a?(Sexp)
       if exp.first == :if
         yield exp
       elsif nested_sexp = exp.enum_for(:deep_each).find { |s| s.first == :if }
