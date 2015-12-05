d2c1b26af2385e6164774d30f626b7df0f1d1c01
Ryan Buckley <arebuckley@gmail.com>
2015-12-04 17:22:13 -1000

    cleanup spec_helper a bit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,13 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'byebug'
 require 'drg'
 require 'sexp_processor'
-dir_pattern = File.expand_path('./spec/fixtures/**/*.rb')
-Dir[dir_pattern].sort.each &method(:require)
+
+FIXTURE_ROOT = Pathname.new(File.join File.expand_path('..', __FILE__), 'fixtures')
+Dir[
+  FIXTURE_ROOT.join('**', '*.rb')
+].sort.each &method(:require)
 
 RSpec.configure do |config|
   config.raise_errors_for_deprecations!
   # config.filter_run focus: true
 end
-
-FIXTURE_ROOT = Pathname.new(File.join File.expand_path('..', __FILE__), 'fixtures')
